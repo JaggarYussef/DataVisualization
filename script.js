77a559b42640a5cd4ae990dfb151660ffe97849d
JaggarYussef <jaggaryussef@gmail.com>
2022-06-24 12:28:06 +0200

    table one now has a multi line chart

--- script.js
+++ script.js
@@ -1,99 +1,113 @@
+// ------------------- GLOBAL VARS ------------------------------------
+let dataT= [];
+let dummy= [80, 100, 56, 120, 180, 30, 40, 120, 160]
+let table = document.getElementById("table1");
 
 
+//-------------------- FUNCTIONS ------------------------------------
 
-let data= [];
-//let dummy= [80, 100, 56, 120, 180, 30, 40, 120, 160]
-//let dummy= [1, 2, 3, 4, 4, 5, 6, 7, 8]
+function drawChart(data){
+   
+    var svgWidth = 600, svgHeight = 400;
+    var margin = { top: 20, right: 20, bottom: 30, left: 50 };
+    var width = svgWidth - margin.left - margin.right;
+    var height = svgHeight - margin.top - margin.bottom;
+    
+    var svg = d3.select('.bar-chart')
+        .attr("width", svgWidth)
+        .attr("height", svgHeight);
+        
+    var g = svg.append("g")
+        .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+    
+    var x = d3.scaleTime()
+        .rangeRound([0, width]);
+    
+    var y = d3.scaleLinear()
+        .rangeRound([height, 0]);
+    
+    var line = d3.line()
+        .x(function(d) { return x(d.year)})
+        .y(function(d) { return y(parseInt(d.value))})
+        x.domain(d3.extent(data, function(d) {   
+             return  d.year;
+             }));
+        
+        y.domain(d3.extent(data, function(d) {
+         //   console.log(" this id date frim inside y " + d.value);    
+            return parseInt(d.value) }));
+
+            g.append("g")
+            .attr("transform", "translate(0," + height + ")")
+            .call(d3.axisBottom(x))
+            .select(".domain")
+            .remove();
+        
+        g.append("g")
+            .call(d3.axisLeft(y))
+            .append("text")
+            .attr("fill", "#000")
+            .attr("transform", "rotate(-90)")
+            .attr("y", 6)
+            .attr("dy", "0.71em")
+            .attr("text-anchor", "end")
+            .text("Price ($)");
+        
+        g.append("path")
+            .datum(data)
+            .attr("fill", "none")
+            .attr("stroke", "steelblue")
+            .attr("stroke-linejoin", "round")
+            .attr("stroke-linecap", "round")
+            .attr("stroke-width", 1.5)
+            .attr("d", line);
+}    
+
 
 function pullData(){
-    var table = document.getElementById("table1");
 
 for (let i = 2; i < table.rows.length; i++  ){
     let row= table.rows[i];
    
     let tempArray= []
-
-    for (let j = 0; j< row.cells.length; j++) {
+    let xVal= 2002;
+ 
+    for (let j = 2; j< row.cells.length; j++) {
+      
         let col = row.cells[j]
         if (col.nodeName == 'TD'){
-            tempArray.push(col.innerText);     
-         }
-    
+           // console.log("outer inner log" + col);
+            tempArray.push({
+                country: row.cells[1].innerText,
+                value: col.innerText,
+                year: xVal++
+            });     
+         }   
     }
-    data.push(tempArray);
-  
-}
-
-}
+    dataT.push(tempArray);
+}}
 
 (()=> {
 
 pullData();   
 // --------------- FIRST GRAPH ELEMENT--------------------   
-let first_DIV= document.createElement("div");
-let firstHeadline= document.querySelector('.mw-headline');
-first_DIV.setAttribute("class", "testDiv h-25");
-first_DIV.style.backgroundColor= 'white';
-first_DIV.style.height= '80vh'
-first_DIV.style.width= '100%';
+
+ let firstHeadline= document.querySelector('.mw-headline');
+
+//-------------- TABLE 1 SVG ----------------------------
+
+let data3= dataT[2]
+console.log('this content of data 3 ' + JSON.stringify(data3.slice(1, data3.length), null, 2   ));
+
+let svgOne= document.createElementNS('http://www.w3.org/2000/svg', 'svg')
+svgOne.setAttribute("class", "bar-chart");
+
+table.before(svgOne);
+
+drawChart(data3)
+drawChart(dataT[4])
 
 
-//-------------- SVG ELEMENT----------------------------
-const svgWidth= "200" ; const svgHeight= "800" ; const barPadding= "5";
-const barWidth= (svgWidth / data[3].length); 
-let data3= data[3]
-console.log("this third type of" +  typeof data3[5] );
-
-//let svgOne= document.createElement("svg");
-//first_DIV.appendChild(svgOne);
-//svgOne.setAttribute("class", "bar-chart");
-let svgRep= d3.select('svg').attr("width", svgWidth).attr("height", svgHeight);
-//svgRep.style("backgroundColor", "black")
-
-
-let bars= svgRep.selectAll("rect")
-    .data(data[3].slice(1,2))
-    .enter()
-    .append("rect")
-    .attr("y", function(d){
-        return svgHeight - (parseInt(d)) / 2 ;
-    })
-    .attr("height", function(d){
-        return d;
-    })
-    .attr("width", barWidth - barPadding)
-    .attr("transform", function(d, i){
-        var translate = [barWidth * i, 0];
-        return "translate("+ translate +")";
-    });
-   
- //  svgRep.appendChild(bars) 
-
-
-
-
-
-//-------------- DISPLAY ----------------------------------
-firstHeadline.after(first_DIV)
-
-
-
-
-
-
-// function cleanUp(array){
-//     for(let i =0; i < array.length; i++){
-//         if(array[i].length == 0){
-
-//             array.splice(i);
-//             console.log(data);
-//         }
-//     }
-// }
-
-
-
-
-console.log(data);
-
+console.log(dataT);
 })();
+
