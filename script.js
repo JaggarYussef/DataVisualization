2dc5601daff5c650065083b80e47bd6d73bd2bcd
JaggarYussef <jaggaryussef@gmail.com>
2022-06-24 16:43:15 +0200

    added divs

--- script.js
+++ script.js
@@ -2,66 +2,93 @@
 let dataT= [];
 let dummy= [80, 100, 56, 120, 180, 30, 40, 120, 160]
 let table = document.getElementById("table1");
-
-
+let g;
+let y;
+let x;
+let line;
 //-------------------- FUNCTIONS ------------------------------------
+Dimensions = {
+  svgWidth: 600,
+  svgHeight: 400,
+  marginTop: 20,
+  marginLeft: 50,
+  marginBottom: 30,
+  marginRight: 20,
+}
+
+
+
+const width= Dimensions.svgWidth - Dimensions.marginLeft - Dimensions.marginRight;
+const height= Dimensions.svgHeight - Dimensions.marginTop -  Dimensions.marginBottom;
+console.log(width);
 
 function drawChart(data){
    
-    var svgWidth = 600, svgHeight = 400;
-    var margin = { top: 20, right: 20, bottom: 30, left: 50 };
-    var width = svgWidth - margin.left - margin.right;
-    var height = svgHeight - margin.top - margin.bottom;
-    
-    var svg = d3.select('.bar-chart')
-        .attr("width", svgWidth)
-        .attr("height", svgHeight);
-        
-    var g = svg.append("g")
-        .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-    
-    var x = d3.scaleTime()
-        .rangeRound([0, width]);
-    
-    var y = d3.scaleLinear()
-        .rangeRound([height, 0]);
-    
-    var line = d3.line()
-        .x(function(d) { return x(d.year)})
-        .y(function(d) { return y(parseInt(d.value))})
-        x.domain(d3.extent(data, function(d) {   
-             return  d.year;
-             }));
-        
-        y.domain(d3.extent(data, function(d) {
-         //   console.log(" this id date frim inside y " + d.value);    
-            return parseInt(d.value) }));
+  // var svgWidth = 600, svgHeight = 400;
+  // var margin = { top: 20, right: 20, bottom: 30, left: 50 };
+  // var width = svgWidth - margin.left - margin.right;
+  // var height = svgHeight - margin.top - margin.bottom;
+  
+  svg = d3.select('.bar-chart')
+      .attr("width", Dimensions.svgWidth)
+      .attr("height", Dimensions.svgHeight);
+      
+   g = svg.append("g")
+      .attr("transform", "translate(" + Dimensions.marginLeft + "," + Dimensions.marginTop + ")");
+  
+   x = d3.scaleTime()
+      .rangeRound([0, width]);
+  
+   y = d3.scaleLinear()
+      .rangeRound([height, 0]);
+  
+g.append("g")
+    .call(d3.axisBottom(x))
+    .attr("transform", "translate(0," + height + ")")
+    .attr("fill", "#000")
+    .attr("x", 6)
+    .attr("dy", "0.71em")
+    .attr("text-anchor", "end")
+    .select(".domain")
+    .remove();
 
-            g.append("g")
-            .attr("transform", "translate(0," + height + ")")
-            .call(d3.axisBottom(x))
-            .select(".domain")
-            .remove();
-        
-        g.append("g")
-            .call(d3.axisLeft(y))
-            .append("text")
-            .attr("fill", "#000")
-            .attr("transform", "rotate(-90)")
-            .attr("y", 6)
-            .attr("dy", "0.71em")
-            .attr("text-anchor", "end")
-            .text("Price ($)");
-        
-        g.append("path")
-            .datum(data)
-            .attr("fill", "none")
-            .attr("stroke", "steelblue")
-            .attr("stroke-linejoin", "round")
-            .attr("stroke-linecap", "round")
-            .attr("stroke-width", 1.5)
-            .attr("d", line);
+g.append("g")
+    .call(d3.axisLeft(y))
+    .append("text")
+    .attr("fill", "#000")
+    .attr("transform", "rotate(-90)")
+    .attr("y", 6)
+    .attr("dy", "0.71em")
+    .attr("text-anchor", "end")
+    .text("Crimes recorded");
+ 
 }    
+
+function drawLine(data){
+  line = d3.line()
+ .x(function(d) { return x(d.year)})
+ .y(function(d) { return y(parseInt(d.value))})
+ x.domain(d3.extent(data, function(d) {   
+      return  d.year;
+      }));
+ 
+ y.domain(d3.extent(data, function(d) {
+  //   console.log(" this id date frim inside y " + d.value);    
+     return parseInt(d.value) }));
+ 
+ 
+ 
+ g.append("path")
+     .datum(data)
+     .attr("fill", "none")
+     .attr("stroke", "steelblue")
+     .attr("stroke-linejoin", "round")
+     .attr("stroke-linecap", "round")
+     .attr("stroke-width", 1.5)
+     .attr("d", line);
+ }
+ 
+ 
 
 
 function pullData(){
@@ -92,20 +119,39 @@ for (let i = 2; i < table.rows.length; i++  ){
 pullData();   
 // --------------- FIRST GRAPH ELEMENT--------------------   
 
- let firstHeadline= document.querySelector('.mw-headline');
-
+ 
 //-------------- TABLE 1 SVG ----------------------------
+let svgOne= document.createElementNS('http://www.w3.org/2000/svg', 'svg')
+svgOne.setAttribute("class", "bar-chart");
+
+let firstHeadline= document.querySelector('.mw-headline');
+ let graphContainer= document.createElement('div')
+ graphContainer.setAttribute("class", "graph-container")
+ graphContainer.style.display= "flex"
+ graphContainer.style.width= '100%'
+ graphContainer.style.height= '80vh';
+ table.before(graphContainer);
+ graphContainer.appendChild(svgOne)
+
+ //----------------------- labels-------------------------
+ 
+ let labelContainer= document.createElement('div')
+ labelContainer.setAttribute("class", "label-container")
+ labelContainer.style.width= '200px'
+ labelContainer.style.height= '80vh';
+ graphContainer.appendChild(labelContainer)
+ 
 
 let data3= dataT[2]
 console.log('this content of data 3 ' + JSON.stringify(data3.slice(1, data3.length), null, 2   ));
 
-let svgOne= document.createElementNS('http://www.w3.org/2000/svg', 'svg')
-svgOne.setAttribute("class", "bar-chart");
 
-table.before(svgOne);
 
 drawChart(data3)
 drawChart(dataT[4])
+drawLine(data3);
+drawLine(dataT[4])
+drawLine(dataT[5])
 
 
 console.log(dataT);
